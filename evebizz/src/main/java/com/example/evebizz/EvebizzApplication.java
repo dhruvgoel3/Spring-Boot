f28dde245e3c9d2faf92a8ac25ec25645b91da59
dhruvgoel3 <dhruvgoel1100@gmail.com>
2026-02-23 23:24:53 +0530

    Project Push Commit

---
+++ evebizz/src/main/java/com/example/evebizz/EvebizzApplication.java
@@ -0,0 +1,13 @@
+package com.example.evebizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvebizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvebizzApplication.class, args);
+	}
+
+}
---
+++ evebizz/src/test/java/com/example/evebizz/EvebizzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evebizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvebizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
