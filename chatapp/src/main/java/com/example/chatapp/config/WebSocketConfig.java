f44440091e55fcaeab87d2d0e1de6adf34023e45
dhruvgoel3 <dhruvgoel1100@gmail.com>
2026-03-08 22:11:20 +0530

    ChatApp using Spring Boot and Basic React

--- chatapp/src/main/java/com/example/chatapp/config/WebSocketConfig.java
+++ chatapp/src/main/java/com/example/chatapp/config/WebSocketConfig.java
@@ -9,14 +9,21 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
-    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/chat").
+                setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
 
     }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+        // set message broker
+    }
+
+
 }
